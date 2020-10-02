5e5b70c7c011732b050cb471e13556ac9cb4370c
hemathota <hema.thota@gmail.com>
2020-10-02 12:30:04 -0700

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -14,7 +14,7 @@ const users = [];
 const siteConfig = {
     title: 'Hema Thota', // Title for your website.
     tagline: 'Continuously improving, aspiring technologist.',
-    profilePhoto: 'img/profile.jpg',
+    profilePhoto: 'img/profile.png',
     url: 'https://hemathota.github.io', // Your website URL
     baseUrl: '/tech-portfolio/', // Base URL for your project */
     // For github.io type URLs, you would set the url and baseUrl like:
